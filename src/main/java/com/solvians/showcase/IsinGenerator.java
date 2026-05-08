1bcdab23a93967589ca45edb76952d329699741d
Talha Jahangiri Khan <tsjahangiri952@gmail.com>
2026-05-08 15:30:25 +0200

    fix: added logic for random chars

--- src/main/java/com/solvians/showcase/IsinGenerator.java
+++ src/main/java/com/solvians/showcase/IsinGenerator.java
@@ -1,5 +1,7 @@
 package com.solvians.showcase;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class IsinGenerator {
 
     private static final String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -8,5 +10,20 @@ public class IsinGenerator {
     public static String generate() {
         return "";
     }
-    
+
+    public static String generate(ThreadLocalRandom random){
+        StringBuilder body = new StringBuilder(11);
+
+        // 2 random uppercase letters
+        for (int i = 0; i < 2; i++) {
+            body.append(LETTERS.charAt(random.nextInt(LETTERS.length())));
+        }
+
+        // 9 random alphanumeric characters
+        for (int i = 0; i < 9; i++) {
+            body.append(ALPHANUMERIC.charAt(random.nextInt(ALPHANUMERIC.length())));
+        }
+
+        return body.toString();
+    }
 }
