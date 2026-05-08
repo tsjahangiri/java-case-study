ac704f0a2c42233271848108febd73da354fae82
Talha Jahangiri Khan <tsjahangiri952@gmail.com>
2026-05-08 15:24:05 +0200

    fix: create isin generator

---
+++ src/main/java/com/solvians/showcase/IsinGenerator.java
@@ -0,0 +1,12 @@
+package com.solvians.showcase;
+
+public class IsinGenerator {
+
+    private static final String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String ALPHANUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public static String generate() {
+        return "";
+    }
+    
+}
