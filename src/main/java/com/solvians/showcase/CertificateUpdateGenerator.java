d6215e6b6c3b7b2f980ddb6618d2e5ab72c8c41c
Talha Jahangiri Khan <tsjahangiri952@gmail.com>
2026-05-08 16:26:41 +0200

    fix: up

--- src/main/java/com/solvians/showcase/CertificateUpdateGenerator.java
+++ src/main/java/com/solvians/showcase/CertificateUpdateGenerator.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
